a94f3c3dc9cb52ac191d4a50c598e7c15efba25d
Filippo Valsorda <hi@filippo.io>
2021-05-26 13:52:31 +0200

    HomebrewFormula: update age to v1.0.0-rc.2

--- HomebrewFormula/age.rb
+++ HomebrewFormula/age.rb
@@ -7,8 +7,8 @@
 class Age < Formula
   desc "Simple, modern, secure file encryption"
   homepage "https://filippo.io/age"
-  url "https://github.com/FiloSottile/age/archive/v1.0.0-rc.1.zip"
-  sha256 "b9269bc3533fefb1dbbc90cb3683be4d4fa3ea41c1a8e7a3265415b2de26f007"
+  url "https://github.com/FiloSottile/age/archive/v1.0.0-rc.2.zip"
+  sha256 "32fa83100e660efc5ecf75547239917147798c91a277f7e1c502575b591f49da"
   head "https://github.com/FiloSottile/age.git"
 
   depends_on "go" => :build
@@ -17,5 +17,7 @@ class Age < Formula
     mkdir bin
     system "go", "build", "-trimpath", "-o", bin, "-ldflags", "-X main.Version=v#{version}", "filippo.io/age/cmd/..."
     prefix.install_metafiles
+    man1.install "doc/age.1"
+    man1.install "doc/age-keygen.1"
   end
 end
