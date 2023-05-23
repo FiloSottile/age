004b544d838bbf016ee52109ee5e0cdc63bcb2e0
Filippo Valsorda <hi@filippo.io>
2023-05-23 17:36:32 +0200

    plugin: add EncodeX25519Recipient

--- plugin/encode.go
+++ plugin/encode.go
@@ -5,6 +5,7 @@
 package plugin
 
 import (
+	"crypto/ecdh"
 	"fmt"
 	"strings"
 
@@ -52,4 +53,14 @@ func ParseRecipient(s string) (name string, data []byte, err error) {
 	}
 	name = strings.TrimPrefix(hrp, "age1")
 	return name, data, nil
+}
+
+// EncodeX25519Recipient encodes a native X25519 recipient from a
+// [crypto/ecdh.X25519] public key. It's meant for plugins that implement
+// identities that are compatible with native recipients.
+func EncodeX25519Recipient(pk *ecdh.PublicKey) (string, error) {
+	if pk.Curve() != ecdh.X25519() {
+		return "", fmt.Errorf("wrong ecdh Curve")
+	}
+	return bech32.Encode("age", pk.Bytes())
 }
